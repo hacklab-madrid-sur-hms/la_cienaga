b775cbe15223b7e88c1c608ed52c0c8c83830d47
ekth0r <hector@musaik.net>
2021-09-04 00:49:34 +0200

    Añadido plugin de subvenciones

---
+++ la_cienaga/plugins/la_cienaga_subvenciones/__init__.py
@@ -0,0 +1,8 @@
+from la_cienaga import logger
+from la_cienaga.core.abc.plugin import Plugin
+from la_cienaga.plugins.la_cienaga_contratos_publicos.parsers.madrid.parser import MadridParser
+from la_cienaga.plugins.la_cienaga_contratos_publicos.parsers.cantabria.parser import CantabriaParser
+
+class ContratosPublicosPlugin(Plugin):
+    def __init__(self):
+        pass
