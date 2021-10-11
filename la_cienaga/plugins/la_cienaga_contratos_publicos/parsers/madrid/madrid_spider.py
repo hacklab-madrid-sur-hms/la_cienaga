44ff370793e5723c661082004fdfc819841e13ec
Héctor Muñoz <hector@musaik.net>
2021-10-11 16:28:16 +0200

    Madrid parser testing
    Parser de Madrid completo y testing inicial

--- la_cienaga/plugins/la_cienaga_contratos_publicos/parsers/madrid/madrid_spider.py
+++ la_cienaga/plugins/la_cienaga_contratos_publicos/parsers/madrid/madrid_spider.py
@@ -1,5 +1,11 @@
+import locale
+import unicodedata
+from babel.numbers import parse_decimal
+from slugify import slugify
+from datetime import datetime as dt
 from scrapy.http import FormRequest, Request
 from scrapy import Spider
+from la_cienaga.plugins.la_cienaga_contratos_publicos.parsers.madrid.madrid_items import MadridItem, AdjudicacionMadridItem
 
 from la_cienaga import logger
 
@@ -63,5 +69,75 @@ class MadridSpider(Spider):
             yield response.follow(next_url, callback=self.parse)
     
     def parse_contract(self, response):
-        pass
-    
+        locale.setlocale(locale.LC_TIME, 'es_ES')
+        madrid_fields = {}
+        titulo = unicodedata.normalize('NFKD', response.xpath("//div[@id='titulo_cabecera']/h2[@class='tit11gr3']/text()").get().strip())
+        madrid_fields['titulo'] = titulo
+        fecha_convocatoria_raw = response.xpath("//div[@id='titulo_cabecera']/div[@class='txt08gr3c']/text()").get()
+        if fecha_convocatoria_raw:
+            fecha_convocatoria_raw = unicodedata.normalize('NFKD', fecha_convocatoria_raw.strip())
+            fecha_convocatoria = dt.strptime(fecha_convocatoria_raw, 'Convocatoria publicada el %d %B %Y %H:%M').strftime('%Y-%m-%d %H:%M')
+            madrid_fields['fecha_convocatoria'] = fecha_convocatoria
+        estado_raw = response.xpath("//div[@id='cont_int_izdo']/span[@class='txt07nar']/text()").get()
+        if estado_raw:
+            estado = unicodedata.normalize('NFKD', estado_raw.strip())
+            madrid_fields['estado'] = estado
+        fecha_fin_presentacion_raw = response.xpath("//div[@id='cont_int_izdo']/span[@class='txt07gr3']/text()").get()
+        if fecha_fin_presentacion_raw:
+            fecha_fin_presentacion_raw = unicodedata.normalize('NFKD', fecha_fin_presentacion_raw.strip())
+            fecha_fin_presentacion = dt.strptime(fecha_fin_presentacion_raw, 'Fin:%d %B %Y').strftime('%Y-%m-%d')
+            madrid_fields['fecha_fin_presentacion'] = fecha_fin_presentacion
+        
+        fields = {}
+        for list_element in response.xpath("//div[@class='listado']"):
+            for element in list_element.xpath("./ul/li[@class='txt08gr3']"):
+                # Se comprueba si la etiqueta <li> contiene una tabla
+                table_tag = element.xpath("./table[@class='tableAdjudicacion']")
+                s_tabla = len(table_tag)
+                if s_tabla > 0:
+                    # si la contiene, es la tabla de adjudicación y hay que
+                    # tratarla de forma distinta
+                    adj_fields = []
+                    columns = [slugify(header.get(), separator='_') for header in table_tag.xpath("./thead/tr/th[@id='tAdjudicaciones' and @colspan='1']/span/text()")]
+                    suma_adj = 0.0
+                    for row in table_tag.xpath("./tbody/tr"):
+                        row_dict = {}
+                        for idx, value in enumerate(row.xpath("./td")):
+                            val = value.xpath('./text()')
+                            adj_title = columns[idx]
+                            adj_value = unicodedata.normalize('NFKD', val.get().strip()) if val.get() else val.get()
+                            if adj_title == 'importe_adjudicacion_con_iva' or adj_title == 'importe_adjudicacion_sin_iva':
+                                adj_value = float(parse_decimal(adj_value, locale='es'))
+                                if adj_title == 'importe_adjudicacion_con_iva':
+                                    suma_adj = suma_adj + adj_value
+                            if adj_title == 'noofertas':
+                                adj_value = int(adj_value)
+                            row_dict[adj_title] = adj_value
+                        # Generamos los datos de adjudicación a partir del diccionario
+                        row_adj = AdjudicacionMadridItem(row_dict)
+                        adj_fields.append(row_adj)
+                    fields['resultado_adjudicacion'] = {
+                        'adjudicatarios': adj_fields,
+                        'importe_total_adjudicacion': suma_adj
+                    }
+                else:
+                    # Si no, son el resto de campos. Se tratan de la forma estándar.
+                    field_name = element.xpath("./strong/text()").get()
+                    field_values = element.xpath('./text()').getall()
+                    if field_name:
+                        field_title = slugify(field_name, separator='_')
+                        values = ' - '.join(list(filter(lambda item: item,[unicodedata.normalize('NFKD',el.strip()) for el in field_values])))
+                        if field_title == 'compra_publica_innovadora':
+                            values = False if values == 'No' else True
+                        if field_title == 'fecha_limite_de_presentacion_de_ofertas_o_solicitudes_de_participacion':
+                            values = dt.strptime(values, '%d %B %Y  %H:%M').strftime('%Y-%m-%d %H:%M')
+                        if field_title == 'valor_estimado_sin_i_v_a' or field_title == 'presupuesto_base_licitacion_sin_impuestos' or field_title == 'presupuesto_base_licitacion_importe_total':
+                            values = float(parse_decimal(values.split()[0], locale='es'))
+                        if field_title == 'adjudicacion_del_contrato_publicada_el':
+                            values = dt.strptime(values, '%d %B %Y').strftime('%Y-%m-%d')
+                        fields[field_title] = values
+        # Generamos el item a partir del diccionario
+        madrid_fields.update(fields)
+        madrid_item = MadridItem (madrid_fields)
+
+        return madrid_item
---
+++ la_cienaga/plugins/la_cienaga_contratos_publicos/tests/test_madrid_parser.py
@@ -0,0 +1,103 @@
+from scrapy.http import Request, HtmlResponse
+from la_cienaga.plugins.la_cienaga_contratos_publicos.parsers.madrid.madrid_spider import MadridSpider
+import pytest
+import os.path
+
+@pytest.fixture
+def load_4118600_response():
+    url = 'http://www.madrid.org/cs/Satellite?c=CM_ConvocaPrestac_FA&cid=1354881221028&definicion=Contratos+Publicos&idPagina=1224915242285&language=es&op2=PCON&pagename=PortalContratacion%2FPage%2FPCON_contratosPublicos&tipoServicio=CM_ConvocaPrestac_FA'
+    filedir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(filedir, 'resources', '4118600.html'), 'rb') as f:
+        html_content = f.read()
+    
+    request = Request(url)
+    response = HtmlResponse(url=url, request=request, body=html_content)
+
+    return response
+
+@pytest.fixture
+def load_madrid_spider():
+    spider = MadridSpider()
+
+    return spider
+
+def test_4118600(load_4118600_response, load_madrid_spider):
+    item = load_madrid_spider.parse_contract(load_4118600_response)
+    assert len(item) == 23 # Son 23 campos a obtener en este contrato
+    ## Detalle del contrato ##
+    ## Principales
+    assert item['titulo'] == 'Suministro de cementos óseos y accesorios para cementación, biomateriales y diverso fungible para el servicio de traumatología del Hospital Universitario de Fuenlabrada'
+    assert item['fecha_convocatoria'] == '2021-05-26 12:30'
+    assert item['estado'] == 'En tramitación'
+    assert item['fecha_fin_presentacion'] == '2021-06-29'
+    assert item['estado_de_la_licitacion'] == 'Adjudicada'
+    assert item['tipo_publicacion'] == 'Convocatoria anunciada a licitación'
+    assert item['objeto_del_contrato'] == 'Suministro de cementos óseos y accesorios para cementación, biomateriales y diverso fungible para el servicio de traumatología del Hospital Universitario de Fuenlabrada'
+    assert item['codigo_cpv'] == '33184100-4'
+    assert item['compra_publica_innovadora'] == False
+    assert item['numero_de_expediente'] == 'PA SUM 21-024'
+    assert item['referencia'] == '4118600'
+    assert item['tipo_de_contrato'] == 'Suministros'
+    assert item['entidad_adjudicadora'] == 'Consejería de Sanidad - Hospital Universitario de Fuenlabrada'
+    assert item['codigo_nuts'] == 'ES300'
+    assert item['procedimiento_adjudicacion'] == 'Abierto'
+    assert item['valor_estimado_sin_i_v_a'] == 408545.51
+    assert item['presupuesto_base_licitacion_sin_impuestos'] == 151313.15
+    assert item['presupuesto_base_licitacion_importe_total'] == 172263.47
+    assert item['duracion_del_contrato'] == '24 Meses'
+    assert item['fecha_limite_de_presentacion_de_ofertas_o_solicitudes_de_participacion'] == '2021-06-29 14:00'
+    assert item['puntos_de_informacion'] == '* Obtención de documentación e información: - - Hospital Universitario de Fuenlabrada - Área de Suministros, de lunes a viernes, de 09:00 a 14:00 horas. Camino del molino, 2. Fuenlabrada-Madrid 28942. Teléfono: 91 6009624/ 91 6006732. Fax: 91 6006712. - - Correo electrónico: suministros.hflr@salud.madrid.org . - - Dirección de Internet: http://www.hospitaldefuenlabrada.org - - Oficinas de Atención al Ciudadano. - * Presentación de ofertas o de solicitudes de participación: - Presentación electrónica accediendo a la plataforma electrónica de contratación pública del Hospital Universitario de Fuenlabrada en la URL: community.vortal.biz/PRODSTS/Users/Login/Index, donde está disponible la información necesaria. - *Apertura de ofertas: - Hospital Universitario de Fuenlabrada. Camino del molino, 2. 28942 Fuenlabrada-Madrid.'
+    assert item['adjudicacion_del_contrato_publicada_el'] == '2021-09-22'
+    ## Adjudicatarios por lotes
+    assert item['resultado_adjudicacion']['importe_total_adjudicacion'] == 122606
+    adj_info = [{'importe_adjudicacion_con_iva': 12650.0,
+ 'importe_adjudicacion_sin_iva': 11500.0,
+ 'nif_adjudicatario': 'B15205537',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'CMM, S.L.U',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 17963.0,        
+ 'importe_adjudicacion_sin_iva': 16330.0,
+ 'nif_adjudicatario': 'B62162326',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'A2C SUMINISTROS HOSPITALARIOS, S.L.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 15840.0,        
+ 'importe_adjudicacion_sin_iva': 14400.0,
+ 'nif_adjudicatario': 'A28123297',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'SMITH NEPHEW S A.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 11357.5,        
+ 'importe_adjudicacion_sin_iva': 10325.0,
+ 'nif_adjudicatario': 'A81726655',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'ORTO MEDIMATEC S A.U',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 17325.0,
+ 'importe_adjudicacion_sin_iva': 15750.0,
+ 'nif_adjudicatario': 'B84463165',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'MAXILARIA SURGERY S.L.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 16087.5,
+ 'importe_adjudicacion_sin_iva': 14625.0,
+ 'nif_adjudicatario': 'ARTHREX ESPAÑA, S.L.',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'ARTHREX ESPAÑA, S.L.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 10758.0,
+ 'importe_adjudicacion_sin_iva': 9780.0,
+ 'nif_adjudicatario': 'B46012696',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'BAXTER S L.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}, {'importe_adjudicacion_con_iva': 20625.0,
+ 'importe_adjudicacion_sin_iva': 18750.0,
+ 'nif_adjudicatario': 'B29060381',
+ 'nolote': None,
+ 'nombre_o_razon_social_adjudicatario': 'ZIMMER BIOMET SPAIN, S.L.U.',
+ 'noofertas': 13,
+ 'resultado': 'Adjudicado'}]
+    assert item['resultado_adjudicacion']['adjudicatarios'] == adj_info
+    ## ##
