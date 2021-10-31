e55649e49cf1e96d2e909506520f2dd60af2df4c
ekth0r <hector@musaik.net>
2021-10-31 13:03:55 +0100

    - Se limpian importaciones innecesarias en cantabria_parser - Se añaden nuevas dependencias para mongodb - Se genera fichero docker-compose para levantar imágenes mongo y   mongo-express

--- la_cienaga/plugins/la_cienaga_contratos_publicos/parsers/cantabria/cantabria_parser.py
+++ la_cienaga/plugins/la_cienaga_contratos_publicos/parsers/cantabria/cantabria_parser.py
@@ -1,13 +1,5 @@
-import os
-import time
-import calendar
-import requests
-import datetime
-import json
-
 from la_cienaga.core.abc.parser import Parser
 from la_cienaga import logger
-from string import Template
 
 class CantabriaParser(Parser):
     title = 'cantabria'
